1632a07ab57e38f129d6505b23bb1b01fd72ee2a
raamganesh <m.p.raamganesh@gmail.com>
2022-04-11 01:08:12 +0100

    Added functions to reset user password and delete user details

--- app_auth/main.py
+++ app_auth/main.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, Response, request, flash, redirect, url_for
+from flask import Blueprint, render_template, Response, request, flash, url_for, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_required, current_user
 from . import db
-from .data_model import Insight
+from .data_model import Insight, User
 
 main = Blueprint("main", __name__)
 
@@ -20,6 +21,66 @@ def profile():
     """
     return render_template("profile.html", user_name=current_user.name)
 
+@main.route("/delete_user")
+@login_required
+def delete_user():
+    """ Render template for Deleting user details
+    """
+    return render_template("delete_user.html")
+
+@main.route("/delete_user", methods=["POST"])
+@login_required
+def delete_user_post():
+    """ To delete user details from the mysql db
+    """
+    if request.method == "POST":
+        pwd = request.form["pwd"]
+
+        user = User.query.filter_by(email=current_user.email).first()
+        db.session.commit()
+        if not user or not check_password_hash(user.password, pwd):
+            flash('Please check your login detail and try again.')
+            return redirect(url_for("main.delete_user"))
+        else:
+            result = User.query.filter_by(email=current_user.email).delete()
+            db.session.commit()
+            if not result:
+                flash("User deletion failed. Try Again!!")
+                return redirect(url_for("main.delete_user"))
+            else:
+                return render_template("index.html")
+    return render_template("delete_user.html")
+
+
+@main.route("/reset_pwd")
+@login_required
+def reset_pwd():
+    """ Render template for to reset user password
+    """
+    return render_template("reset_pwd.html")
+
+@main.route("/reset_pwd", methods=["POST"])
+@login_required
+def reset_pwd_post():
+    """ To reset user password detail from the mysql db
+    """
+    if request.method == "POST":
+        newpwd = request.form["newpwd"]
+        newpwd2 = request.form["newpwd2"]
+        if newpwd != newpwd2:
+            flash("Re-type Password")
+            return redirect(url_for("main.reset_pwd"))
+        email = current_user.email
+        new_password = generate_password_hash(newpwd2, method="sha256")
+        result = User.query.filter(User.email==email).update({"password": new_password})
+        db.session.commit()
+        if not result:
+            flash("Password update failed. Try Again!!")
+            return redirect(url_for("main.reset_pwd"))
+        else:
+            flash("Update Successful")
+            return render_template("login.html")
+    return render_template("reset_pwd.html")
 
 @main.route("/profile", methods=["POST"])
 @login_required
